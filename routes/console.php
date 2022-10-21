f2001ac816c7f6555bddbf7e97c4f13ff9e43e0e
Mauricio <80491400+mauriciostei@users.noreply.github.com>
2022-10-21 16:28:16 -0300

    Clear Logs

--- routes/console.php
+++ routes/console.php
@@ -3,17 +3,11 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
-
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('logs:clear', function(){
+    exec('echo "" > '.storage_path('logs/laravel.log'));
+    $this->comment('Logs han sido eliminados');
+})->describe('Eliminar logs de la aplicación');
