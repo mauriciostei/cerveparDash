115ddfdb90efc973f504e5c295a4129903e5a498
Mauricio <80491400+mauriciostei@users.noreply.github.com>
2023-01-27 12:29:07 -0300

    Eliminar puntos adicionales (Control Moviles)

--- resources/views/livewire/dashboards/control-moviles.blade.php
+++ resources/views/livewire/dashboards/control-moviles.blade.php
@@ -51,40 +51,42 @@
 
         <div class="row pt-2">
             @forelse($puntos as $p)
-                <div class="col">
-                    <div class="card">
-                        <div class="card-header text-center">
-                            <h5 data-toggle="tooltip" data-placement="top" title="Tiempo target {{ $p->target }}">
-                                {{$this->getPunto($p->puntos_id)}}
-                            </h5>
-                        </div>
-                        <div class="card-body">
-                            <div class="row">
-                                @forelse($recorridos->where('puntos_id', $p->puntos_id) as $r)
-                                    <div class="col text-center text-bold text-lg">
-                                        <span data-toggle="tooltip" data-placement="top" title="Hora de inicio {{ $r->inicio }}, hora target {{ $r->target }}">
-                                            @switch($r->estado)
-                                                @case('OnTime')
-                                                    <span class="text-success"> {{$r->moviles->nombre}} </span>
-                                                @break
-                                                @case('OutOfTime')
-                                                    <span class="text-danger"> {{$r->moviles->nombre}} </span>
-                                                @break
-                                                @case('Dismiss')
-                                                    <span class="text-warning"> {{$r->moviles->nombre}} </span>
-                                                @break
-                                            @endswitch
-                                        </span>
-                                    </div>
-                                @empty
-                                    <div class="col text-muted text-center">
-                                        Sin móviles circulando
-                                    </div>
-                                @endforelse
+                @if($p != $loop->last)
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-header text-center">
+                                <h5 data-toggle="tooltip" data-placement="top" title="Tiempo target {{ $p->target }}">
+                                    {{$this->getPunto($p->puntos_id)}}
+                                </h5>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    @forelse($recorridos->where('puntos_id', $p->puntos_id) as $r)
+                                        <div class="col text-center text-bold text-lg">
+                                            <span data-toggle="tooltip" data-placement="top" title="Hora de inicio {{ $r->inicio }}, hora target {{ $r->target }}">
+                                                @switch($r->estado)
+                                                    @case('OnTime')
+                                                        <span class="text-success"> {{$r->moviles->nombre}} </span>
+                                                    @break
+                                                    @case('OutOfTime')
+                                                        <span class="text-danger"> {{$r->moviles->nombre}} </span>
+                                                    @break
+                                                    @case('Dismiss')
+                                                        <span class="text-warning"> {{$r->moviles->nombre}} </span>
+                                                    @break
+                                                @endswitch
+                                            </span>
+                                        </div>
+                                    @empty
+                                        <div class="col text-muted text-center">
+                                            Sin móviles circulando
+                                        </div>
+                                    @endforelse
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                @endif
             @empty
                 <div class="text-muted text-center">
                     Sin datos presentes
