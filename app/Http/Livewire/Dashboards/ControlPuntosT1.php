d8597d109561de922c0127a471580a1c0f8a940b
Mauricio <80491400+mauriciostei@users.noreply.github.com>
2024-02-22 17:35:11 -0300

    Exportacion del control T1

--- app/Http/Livewire/Dashboards/ControlPuntosT1.php
+++ app/Http/Livewire/Dashboards/ControlPuntosT1.php
@@ -5,11 +5,14 @@ namespace App\Http\Livewire\Dashboards;
 use App\Models\Puntos;
 use App\Models\Recorridos;
 use App\Models\Tiers;
+use App\Traits\ExportarExcel;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class ControlPuntosT1 extends Component
 {
+    use ExportarExcel;
+
     public $tiers_id = 1;
     public $viaje = 1;
     public $fecha;
@@ -40,12 +43,37 @@ class ControlPuntosT1 extends Component
         return $r->HoraInicio ?? null;
     }
 
+    public function exportar(){
+        $this->getInfo();
+        $columns = array('Fecha','Movil', 'Chofer');
+        foreach($this->puntos as $punto):
+            $columns[] = $this->getPunto($punto->puntos_id);
+        endforeach;
+
+        $datos = Array();
+        foreach($this->recorridos as $recorrido):
+            $line = Array(
+                $this->fecha,
+                $recorrido['moviles_nombre'],
+                strtoupper($recorrido['choferes_nombre']),
+            );
+
+            foreach($this->puntos as $punto):
+                $line[] = $this->getRecorrido($recorrido['moviles_id'], $punto->puntos_id) ?? 'X';
+            endforeach;
+
+            $datos[] = $line;
+        endforeach;
+
+        return $this->getFile('ControlPuntosT1.xls', $columns, $datos);
+    }
+
     public function getInfo(){
         $this->tiers = Tiers::all();
         $this->puntos = DB::table('puntos_tiers')->where('tiers_id', $this->tiers_id)->where('viaje', $this->viaje)->orderBy('orden', 'asc')->get();
 
         $this->recorridos = Recorridos::select([DB::raw("string_agg(distinct choferes.nombre::text, ',') as choferes_nombre"), 'moviles.nombre as moviles_nombre','recorridos.moviles_id as moviles_id'])
-        ->join('choferes', 'recorridos.choferes_id', '=', 'choferes.id')
+            ->join('choferes', 'recorridos.choferes_id', '=', 'choferes.id')
             ->join('moviles', 'recorridos.moviles_id', '=', 'moviles.id')
             ->whereDate('inicio', $this->fecha)
             ->where('viaje', $this->viaje)
