b7c83a69e5907d404c0cf75e40345d0680b6aac6
Mauricio <80491400+mauriciostei@users.noreply.github.com>
2024-02-28 09:33:14 -0300

    BIoStar Try

--- app/Traits/BioStarData.php
+++ app/Traits/BioStarData.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits;
 
+use Error;
 use Illuminate\Support\Facades\Http;
 
 trait BioStarData{
@@ -14,68 +15,72 @@ trait BioStarData{
         $pass = env("SW_PASS");
         $evento = env("SW_EVENT");
 
-        // Ingresar usuario al sistema y obtener token
-        $request = Http::withBody('{"User":{"login_id":"'.$user.'","password":"'.$pass.'"}}', "application/json")
-        ->withoutVerifying()
-        ->withHeaders([
-            'accept' => 'application/json'
-        ])
-        ->post($url."login");
+        
+        try{
+          // Ingresar usuario al sistema y obtener token
+          $request = Http::withBody('{"User":{"login_id":"'.$user.'","password":"'.$pass.'"}}', "application/json")
+          ->withoutVerifying()
+          ->withHeaders([
+              'accept' => 'application/json'
+          ])
+          ->post($url."login");
 
-        $token = $request->headers()['bs-session-id'][0];
+          $token = $request->headers()['bs-session-id'][0];
 
-        // Obtener lista de usuarios Leidos en 5 segundos
-        $request = Http::
-        withBody('
-        {
-            "Query": {
-              "limit": 50,
-              "conditions": [
-                {
-                  "column": "datetime",
-                  "operator": 3,
-                  "values": [
-                    "'.$this->setTime($inicio).'",
-                    "'.$this->setTime($fin).'"
-                  ]
-                },
-                {
-                  "column": "event_type_id.code",
-                  "operator": 2,
-                  "values": '.$evento.'
-                },
-                {
-                  "column": "user_id.user_id",
-                  "operator": 1,
-                  "values": ["1"]
-                }
-              ],
-              "orders": [
-                {
-                  "column": "datetime",
-                  "descending": false
-                }
-              ]
+          // Obtener lista de usuarios Leidos en 5 segundos
+          $request = Http::
+          withBody('
+          {
+              "Query": {
+                "limit": 50,
+                "conditions": [
+                  {
+                    "column": "datetime",
+                    "operator": 3,
+                    "values": [
+                      "'.$this->setTime($inicio).'",
+                      "'.$this->setTime($fin).'"
+                    ]
+                  },
+                  {
+                    "column": "event_type_id.code",
+                    "operator": 2,
+                    "values": '.$evento.'
+                  },
+                  {
+                    "column": "user_id.user_id",
+                    "operator": 1,
+                    "values": ["1"]
+                  }
+                ],
+                "orders": [
+                  {
+                    "column": "datetime",
+                    "descending": false
+                  }
+                ]
+              }
             }
+          ', 'application/json')
+          ->withoutVerifying()
+          ->withHeaders([
+              'accept' => 'application/json',
+              'bs-session-id' => $token,
+          ])
+          ->post($url."events/search");
+
+          // Eliminar el token de la aplicación
+          Http::withoutVerifying()->withHeaders(['bs-session-id' => $token])->post($url."logout");
+
+          if($request->successful()){
+              $response = json_decode($request->body());
+              return $response->EventCollection->rows;
+          }else{
+              return null;
           }
-        ', 'application/json')
-        ->withoutVerifying()
-        ->withHeaders([
-            'accept' => 'application/json',
-            'bs-session-id' => $token,
-        ])
-        ->post($url."events/search");
-
-        // Eliminar el token de la aplicación
-        Http::withoutVerifying()->withHeaders(['bs-session-id' => $token])->post($url."logout");
-
-        if($request->successful()){
-            $response = json_decode($request->body());
-            return $response->EventCollection->rows;
-        }else{
-            return null;
+        }catch(Error $error){
+          return null;
         }
-
     }
 
     private function setTime($time){
