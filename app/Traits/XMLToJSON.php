ef498aae405ffebd6284dbeaf41ac9804a743ecd
mauriciostei <mauriciostei@hotmail.com>
2025-10-31 20:42:57 -0300

    Evitar error por xml vacio

--- app/Traits/XMLToJSON.php
+++ app/Traits/XMLToJSON.php
@@ -5,13 +5,17 @@ namespace App\Traits;
 trait XMLToJSON{
 
     public function xmlToJson($request){
-        $file = $request->file('anpr_xml');
-        $fo = fopen($file, 'r');
-        $contenido = fread($fo, filesize($file));
-        fclose($fo);
+        try {
+            $file = $request->file('anpr_xml');
+            $fo = fopen($file, 'r');
+            $contenido = fread($fo, filesize($file));
+            fclose($fo);
 
-        libxml_use_internal_errors(true);
-        return simplexml_load_string($contenido);
+            libxml_use_internal_errors(true);
+            return simplexml_load_string($contenido);
+        } catch(Exception $e) {
+            return false;
+        }
     }
 
 }
